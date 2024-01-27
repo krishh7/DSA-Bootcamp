dc4b99b9ef3c5041e79de9c3f2350296ba221797
Krishna Torrem <krishnatorrem@gmail.com>
2024-01-27 14:32:58 +0530

    Solve array through bubble sort

---
+++ src/main/java/org/example/bubbleSort/LearnBubbleSort.java
@@ -0,0 +1,52 @@
+package org.example.bubbleSort;
+
+import java.util.Arrays;
+
+public class LearnBubbleSort {
+    public static void main(String[] args) {
+        int[] array = {15, 23, 43, 32, 1, 2, 45, 12};
+        int[] array2 = {1, 2, 3, 4, 5, 6};
+        bubbleSort(array);
+        System.out.println(Arrays.toString(array));
+        efficientBubbleSort(array2);
+        System.out.println(Arrays.toString(array2));
+    }
+
+    private static void bubbleSort(int[] array) {
+        //run the steps for n-1 times
+        for (int i = 0; i < array.length; i++) {
+            //for each step the max num will come at the last respective index
+            for (int j = 1; j < array.length - i; j++) {
+                //swap if the num is smaller than previous item
+                if (array[j] < array[j-1]) {
+                    int temp = array[j];
+                    array[j] = array[j-1];
+                    array[j-1] = temp;
+                }
+            }
+        }
+    }
+
+    /*
+    The above method has a minor problem that in case of a sorted array given like
+    [1, 2, 3, 4, 5] the swap will never happen but the loop runs for i = 0 to i = N-1
+    To avoid that we knew if the swap not happened for any i = something then that array has been sorted.
+    Meaning we can stop the loop.
+     */
+    private static void efficientBubbleSort(int[] array) {
+        boolean swapped = false;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < array.length - i; j++) {
+                if (array[j] < array[j-1]) {
+                    int temp = array[j];
+                    array[j] = array[j-1];
+                    array[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
