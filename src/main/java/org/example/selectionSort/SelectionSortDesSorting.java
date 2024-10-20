e53c21cb2a4e45e29f27cfbee50218e2c3dce80e
Krishna Torrem <krishnarao.torrem@nike.com>
2024-10-20 21:21:17 +0530

    remove space

--- src/main/java/org/example/selectionSort/SelectionSortDesSorting.java
+++ src/main/java/org/example/selectionSort/SelectionSortDesSorting.java
@@ -16,7 +16,6 @@ public class SelectionSortDesSorting {
         for (int i = 0; i < n - 1; i++) {
             int maxIdx = findMax(numbers, i, n - 1);
             swap(numbers, i, maxIdx);
-
         }
     }
 
